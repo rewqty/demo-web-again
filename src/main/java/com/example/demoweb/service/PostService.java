1c75eca8906a9b7ded3f178ff18a121130f64ba1
rewqty <dmitriy.chelnokov.0000@gmail.com>
2022-10-18 18:28:31 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,14 +4,15 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class PostService {
     public ArrayList<Post> listAllPosts() {
         ArrayList<Post> listPosts = new ArrayList<>();
-        listPosts.add(new Post("first"));
-        listPosts.add(new Post("second"));
-        listPosts.add(new Post("third"));
+        listPosts.add(new Post("first", new Date()));
+        listPosts.add(new Post("second", new Date()));
+        listPosts.add(new Post("third", new Date()));
         return listPosts;
     }
 }
