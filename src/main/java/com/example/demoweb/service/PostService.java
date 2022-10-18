f4acac630e2e0e99217760562126576d723ec5e7
rewqty <dmitriy.chelnokov.0000@gmail.com>
2022-10-18 19:22:50 +0500

    create

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,15 +4,25 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 @Service
 public class PostService {
+
+    private ArrayList<Post> posts = new ArrayList<>(
+            Arrays.asList(
+                    new Post("first", new Date()),
+                    new Post("second", new Date()),
+                    new Post("third", new Date())
+            )
+    );
+
     public ArrayList<Post> listAllPosts() {
-        ArrayList<Post> listPosts = new ArrayList<>();
-        listPosts.add(new Post("first", new Date()));
-        listPosts.add(new Post("second", new Date()));
-        listPosts.add(new Post("third", new Date()));
-        return listPosts;
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
