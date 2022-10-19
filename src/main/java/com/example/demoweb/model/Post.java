4ce373928e1531a6e258ebc4f6973b328d13361e
rewqty <dmitriy.chelnokov.0000@gmail.com>
2022-10-19 23:10:44 +0500

    JavaScript

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -6,8 +6,10 @@ public class Post {
     private String text;
     private Integer likes;
     private Date creationDate;
+    private Long id;
 
-    public Post(String text, Date creationDate) {
+    public Post(Long id, String text, Date creationDate) {
+        this.id = id;
         this.text = text;
         this.likes = 0;
         this.creationDate = creationDate;
@@ -23,5 +25,13 @@ public class Post {
 
     public Date getCreationDate() {
         return creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -12,9 +12,9 @@ public class PostService {
 
     private ArrayList<Post> posts = new ArrayList<>(
             Arrays.asList(
-                    new Post("first", new Date()),
-                    new Post("second", new Date()),
-                    new Post("third", new Date())
+                    new Post(0L,"first", new Date()),
+                    new Post(1L, "second", new Date()),
+                    new Post(2L, "third", new Date())
             )
     );
 
@@ -23,6 +23,6 @@ public class PostService {
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        posts.add(new Post((long) posts.size(), text, new Date()));
     }
 }
